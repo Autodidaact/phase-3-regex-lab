d06e5a9a44b522667c9d06bccc11b63cbd684f5f
Ibrahim Abdulai Yakubu <ibrahimay295@gmail.com>
2022-08-23 13:11:06 +0000

    commit tree d80b202ed0ceaada56939f4fc3c620e4ddbcff72

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,18 @@
+require "pry"
 def starts_with_a_vowel?(word)
-
+    if /^[^aeiou\W]/i.match(word) == nil
+        return true
+    else
+        return false
+    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/\b[Uu]n[a-z]*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+    preg_grep("/^.{5}$/", $text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
@@ -17,3 +22,4 @@ end
 def valid_phone_number?(phone)
 
 end
+
